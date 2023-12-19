cf5c1e5892edb90e6380dbc0f8448486dec1628d
AkshayaN99 <akspra9@gmail.com>
2023-12-19 21:18:33 +0530

    Test commit

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Navbar from './components/navbar';
+import Heroimage from './components/hero';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Heroimage/>
     </div>
   );
 }
---
+++ qtify/src/components/hero.jsx
@@ -0,0 +1,44 @@
+import headphones from "../assets/headphones.png";
+import React from "react";
+import styles from "./hero.module.css";
+ 
+
+function Heroimage(){
+
+return (
+
+<div className={styles.rectangle}>
+
+<div className={styles.htext}>
+
+<p>100 Thousand Songs, ad-free</p>
+
+<p>Over thousands podcast episodes</p>
+
+</div>
+<div className={styles.hlogo} >
+
+<img src={headphones} width={212}  alt="headphones"/>
+</div>
+
+</div>
+
+)
+
+}
+
+ 
+
+export default Heroimage;
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
